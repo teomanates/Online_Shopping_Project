5fefac13b0fa7430cf7d0a1cd43d44573c5514fb
teoman ates <teomanates1@gmail.com>
2024-08-13 23:55:21 +0300

    commit

--- product-catalog-service/src/main/java/com/shopping/product_catalog_service/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shopping.product_catalog_service.repository;
-
-import com.shopping.product_catalog_service.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
